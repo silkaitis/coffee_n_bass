a881baa3adbe058444bd8989ec1e63d6ddad2d1f
Danius Silkaitis <danius.silkaitis@gmail.com>
2017-01-15 10:31:49 -0800

    Mixcloud class added

--- src/dnb_classes.py
+++ src/dnb_classes.py
@@ -2,6 +2,7 @@ import yaml
 
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 class tracklister(object):
     '''
@@ -581,6 +582,8 @@ class mixcloud(object):
             .find_element_by_xpath('''//div[@class='user-actions']/a[contains(text(),'Log in')]''') \
             .click()
 
+        sleep(0.5)
+
         self.driver \
             .find_element_by_xpath('''//input[@ng-model='formData.email']''') \
             .send_keys(self.user)
@@ -592,6 +595,8 @@ class mixcloud(object):
         self.driver \
             .find_element_by_xpath('''//button[@class='btn btn-big btn-secondary']''') \
             .click()
+
+        sleep(1)
 
         return
 
@@ -610,7 +615,7 @@ class mixcloud(object):
         '''
         Select file to upload
         '''
-        loc = '/Users/danius/Downloads' + self.show_filename
+        loc = '/Users/danius/Downloads/' + self.show_filename
 
         self.driver \
             .find_element_by_xpath('''//input[@type='file']''') \
@@ -626,7 +631,7 @@ class mixcloud(object):
 
         self.driver \
             .find_element_by_xpath('''//input[@id='cloudcast-name']''') \
-            .send_keys(title)
+            .send_keys('coffee n bass : ' + title)
 
         return
 
@@ -639,17 +644,61 @@ class mixcloud(object):
             .click()
 
         #Waiting long enough to ensure the file uploaded
-        sleep(10 * 60)
+        #sleep(60 * 3)
 
         return
-        
-    def enter_track_list(self):
+
+    def initialize_track_list(self):
         '''
-        Enter track list into text box
+        Go to tracklist window and prep it for data entry
+        '''
+        tlist_window = self.driver \
+                           .find_elements_by_xpath('''//textarea[@class='tracklist-textarea ng-pristine ng-valid']''')
+
+        self.scroll_to_element(tlist_window)
+
+        #Rough work around because sending command+v doesn't work
+        tlist_window.click()
+        tlist_window.send_keys('t')
+
+        return
+
+    def prep_track_list(self):
+        '''
+        Clear out the initialization of the tracklist
         '''
         self.driver \
-            .find_element_by_name('frm_tracklist') \
-            .send_keys(self.track_list)
+            .find_element_by_xpath('''//textarea[@m-field-type='artist']''') \
+            .clear()
+
+        return
+
+    def enter_track_list(self):
+        '''
+        Enter track list for the show
+        '''
+        self.initialize_track_list()
+
+        self.prep_track_list()
+
+        for i, trk in enumerate(self.track_list):
+            trk = trk.replace('\r\n', '').split(' - ')
+
+            if len(trk) > 2:
+                del t[2:]
+
+            if i < len(self.track_list) - 1:
+                t[1] = t[1] + '\n'
+
+            act2 = ActionChains(self.driver)
+
+            act2.send_keys(t[0] + '\t')
+            act2.send_keys(t[1])
+            act2.perform()
+
+            del act2
+
+            sleep(0.5)
 
         return
 
@@ -658,8 +707,37 @@ class mixcloud(object):
         Enter path for show's image
         '''
         self.driver \
-            .find_element_by_name('frm_picture') \
-            .send_keys('/Users/danius/Documents/ritchey_cover_05_-_small_4.jpg')
+            .find_element_by_name('''//input[@m-file-input-model='cloudcastEdit.picture']''') \
+            .send_keys('/Users/danius/Documents/podcast cover 1.JPG')
+
+        return
+
+    def add_tags(self):
+        '''
+        Enter tags for the show
+        '''
+        tags = ['Liquid Drum and Bass,',
+                'Drum & Bass,',
+                'Drum and Bass,',
+                'Live,',
+                'Seattle,']
+
+        tag_elem = self.driver \
+                       .find_element_by_xpath('''//input[@ng-model='newToken']''')
+
+        for t in tags:
+            tag_elem.send_keys(t)
+
+        return
+
+    def scroll_to_element(self, element):
+        '''
+        Scroll till element is within view
+        '''
+        loc = element.location_once_scrolled_into_view
+
+        self.driver \
+            .execute_script("window.scrollTo(0, " + str(loc['y']) + ");")
 
         return
 
@@ -667,41 +745,14 @@ class mixcloud(object):
         '''
         Save edits made to show page
         '''
-        self.driver \
-            .find_element_by_xpath("//input[@value='Save']") \
-            .click()
+        element = self.driver \
+                      .find_element_by_xpath('''//div[@m-click='save()']''')
+
+        self.scroll_to_element(element)
+
+        element.click()
 
         sleep(5)
-        return
-
-    def bbc_code(self):
-        '''
-        Save the generated BBC code
-        '''
-        self.bbc = self.driver \
-                        .find_element_by_xpath("//textarea") \
-                        .text
-
-        return
-
-    def fetch_filename(self):
-        '''
-        Store show file name
-        '''
-        self.show_filename = self.driver \
-                                 .find_element_by_xpath('//a[contains(text(),"Download this mix")]') \
-                                 .get_attribute('href') \
-                                 .split('livesets/')[1]
-
-        return
-
-    def download_show(self):
-        '''
-        Download the show
-        '''
-        self.driver \
-            .find_element_by_xpath('//a[contains(text(),"Download this mix")]') \
-            .click()
 
         return
 
@@ -713,35 +764,29 @@ class mixcloud(object):
 
         return
 
-    def update_show(self):
+    def publish(self):
         '''
-        Update show with track list and image
+        Publish show with track list, image and tags
         '''
         self.launch()
 
         self.login()
 
-        self.goto_archive()
+        self.goto_upload_page()
 
-        self.goto_latest_show()
+        self.select_file()
 
-        self.goto_edit_page()
+        self.enter_show_name()
 
-        self.enter_track_list()
+        self.upload()
 
         self.enter_show_image()
 
+        self.add_tags()
+
+        self.enter_track_list()
+
         self.save_edits()
-
-        self.goto_archive()
-
-        self.goto_latest_show()
-
-        self.download_show()
-
-        self.fetch_filename()
-
-        self.bbc_code()
 
         self.shutdown()
 
